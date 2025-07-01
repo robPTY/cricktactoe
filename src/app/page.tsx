079ffb7e6950e641da2731e300ff251ecb666630
robPTY <rcadown8@gmail.com>
2025-06-30 21:27:44 -0500

    Initial changes...will do more soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import Image from "next/image";
+import HomePageClient from "@/components/HomePageClient";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        Welcome to CrickTacToe.
-      </main>
-    </div>
-  );
+  return <HomePageClient />;
 }
